19bcd43ca13060ea2221b1e01fdc1e9c84d16c71
Kamizumi <timtsang0513@gmail.com>
2025-09-23 14:07:33 -0700

    Added Tim's url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("hello/", views.hello, name="hello"),
     path("jason/", views.jason_greeting, name="jason_greeting"),
+    path("tim/", views.tim_greeting, name="tim_greeting"),
 ]
